49200bb97eb8fddc010aefcf7f72f0545fe4c58c
Chrysanthi Theocharidi <ctheoc05@ucy.ac.cy>
2025-11-18 18:03:50 +0200

    Limited the Player to stay in the platform

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -6,17 +6,25 @@ public class PlayerMovements : MonoBehaviour
 {
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
+    public float rightLimitt = 5.5f;
+    public float leftLimitt = -5.5f;
+
 
     void Update()    
     {    
         transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime, Space.World); 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))   
         {    
-            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+            if(this.gameObject.transform.position.x > leftLimitt)    
+            {
+                transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+
+            }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {    
-            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+            if(this.gameObject.transform.position.x < rightLimitt)  
+                transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
         }
     }
 
