7485f716b2f87adcb0b238f444dc39c35f2c841d
Chrysanthi Theocharidi <ctheoc05@ucy.ac.cy>
2025-11-18 17:52:48 +0200

    Left and Right Movements

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -4,10 +4,20 @@ using System.Collections.Generic;
 
 public class PlayerMovements : MonoBehaviour    
 {
-    float playerSpeed = 2;
+    public float playerSpeed = 2;
+    public float horizontalSpeed = 3;
+
     void Update()    
     {    
-        transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime, Space.World);    
+        transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime, Space.World); 
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))   
+        {    
+            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {    
+            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+        }
     }
 
 }
