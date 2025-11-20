1098c6eee1e1f53d52d2a599ecae1084c5d8d3b7
IoannisPapayiannis <82267085+IoannisPapayiannis@users.noreply.github.com>
2025-11-20 21:55:02 +0200

    collision

---
+++ Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CollisionDetect :Monobehaviour
+
+{
+	[SerializeField] GameObject thePlayer;
+	void onTriggerEnter(Collider other)
+	{
+		thePlayer.GetComponent<PlayerMovements>().enabled = false;
+	}
+}
