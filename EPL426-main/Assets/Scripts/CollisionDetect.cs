3fcc28e69666e768967aceb7280dca1065ddf6fb
IoannisPapayiannis <82267085+IoannisPapayiannis@users.noreply.github.com>
2025-11-20 21:45:36 +0200

    collision

--- EPL426-main/Assets/Scripts/CollisionDetect.cs
+++ EPL426-main/Assets/Scripts/CollisionDetect.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class CollisionDetect :MonoBehaviour  
+public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     void OnTriggerEnter(Collider other)
     {
         thePlayer.GetComponent<PlayerMovements>().enabled = false;
-    } 
+    }
 
 }
